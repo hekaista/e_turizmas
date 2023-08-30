e6a20fd36dfdea0b9eb16c9547ed1cf2c217e652
Heka <code@epixel.lt>
2023-08-30 18:37:01 +0300

    projektas ir app: settings, urls, importai

--- egidas/views.py
+++ egidas/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+# from django.http import HttpResponse
+from django.views import generic
+# from .models import
+from django.db.models import Q
+from django.core.paginator import Paginator
+
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
